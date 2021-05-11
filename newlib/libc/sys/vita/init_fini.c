d8e307161f0365f9b40090d5c5a1b132805ed947
Jianye Chen <jianye.chen@uwaterloo.ca>
2021-05-11 14:54:04 -0400

    newlib's per-thread initializer should only init itself
    pthread_init will be called by its own initializer instead
    
    Signed-off-by: Jianye Chen <jianye.chen@uwaterloo.ca>

--- newlib/libc/sys/vita/init_fini.c
+++ newlib/libc/sys/vita/init_fini.c
@@ -5,7 +5,9 @@
 
 #include <string.h>
 
-int __psp2cldr_init_newlib(void)
+extern void exit(int);
+int _init_signal(void);
+void __psp2cldr_init_newlib(void)
 {
     /* __getreent currently doesn't know what to return */
     PSP2CLDR_TLS *tls = __psp2cldr__internal_tls_ctrl(0);
@@ -13,13 +15,17 @@ int __psp2cldr_init_newlib(void)
     _REENT_INIT_PTR(&tls->reent);
 
     /* malloc and all other utilities are now up */
-    return pthread_init();
+
+    /* run per-thread init functions */
+    if (_init_signal() != 0)
+    {
+        exit(0x12345678);
+    }
 }
 
-int __psp2cldr_fini_newlib(void)
+void __psp2cldr_fini_newlib(void)
 {
     __psp2cldr__internal_tls_ctrl(1);
-    return 0;
 }
 
 struct _reent *__getreent(void)
